d2c31b8e9368f34b6ab1ab7db4ff051f5b2df76c
lauralindy <laura.f.lin@gmail.com>
2018-02-17 21:45:25 -0500

    some changes idk what

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, send, emit
+import os
+import sys
+sys.path.insert(0, os.getcwd()+"/src")
+import CCRAttendance
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template("index.html", title = "Welcome")
 
+@socketio.on('json')
+def handle_json(json):
+    send(json, json = True)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app)

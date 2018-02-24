f3d53da3697557a687c03a8f6cfe6ece85365b1e
LucasSwitz <lucswitz@gmail.com>
2018-02-24 15:12:06 -0500

    Fixed DB config

--- src/RpiRFID.py
+++ src/RpiRFID.py
@@ -1,4 +1,4 @@
-import MFRC522
+from MFRC522 import MFRC522
 import time 
 
 rfid_read_sleep = 0.001 # seconds
@@ -9,14 +9,14 @@ class RpiRFID:
         self._do_read = True
 
     def init(self):
-        self._reader = MFRC522.MFRC522()
+        self._reader = MFRC522()
 
     def read_value(self):
         while self._do_read:
             time.sleep(rfid_read_sleep)
-            (status,_) = self._reader.MFRC522_Request(MFRC522.MFRC522.PICC_REQIDL)
+            (status,_) = self._reader.MFRC522_Request(MFRC522.PICC_REQIDL)
 
-            if status == MFRC522.MFRC522.MI_OK:
+            if status == MFRC522.MI_OK:
                 (status,uid) = self._reader.MFRC522_Anticoll()
 	    uid_int = 0
             if status == MFRC522.MFRC522.MI_OK:    
